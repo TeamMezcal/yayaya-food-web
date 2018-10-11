0a48c188fa149a176d763ccc57c7c0a6974bd5c6
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-10-11 21:07:30 +0200

    login, regiser components added; session and user services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/misc/header/header.component';
 import { UsersListComponent } from './components/user/users-list/users-list.component';
 import { UserItemComponent } from './components/user/user-item/user-item.component';
+import { LoginComponent } from './components/misc/login/login.component';
+import { RegisterComponent } from './components/misc/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UsersListComponent,
-    UserItemComponent
+    UserItemComponent,
+    LoginComponent,
+    RegisterComponent
     
   ],
   imports: [
